809f04a3ee3be86be114f1e1f1e5025884b46e1f
Harun Tucakovic <harun.tucakovic1@gmail.com>
2021-04-20 23:28:32 +0200

    formmating edits & gitignore update

--- routes/auth.js
+++ routes/auth.js
@@ -15,33 +15,36 @@ const router = express.Router()
 // @desc        Authorize user and get user info
 // @access      Private
 router.get('/', authMiddleware, async (req, res) => {
-    try {
-        const connection = await getConnection()
-        let result = await connection.query("SELECT * FROM app_user WHERE app_user_id = ?", req.user.id)
-        connection.release()
-        let {password, ...responseObj} = result[0][0]
-        return res.status(200).json(responseObj)
-    } catch (error) {
-        console.log(error)
-            return res
-				.status(500)
-				.json({ errors: [{ code: 500, message: 'Server error' }] })
-    }
+	try {
+		const connection = await getConnection()
+		let result = await connection.query(
+			'SELECT * FROM app_user WHERE app_user_id = ?',
+			req.user.id
+		)
+		connection.release()
+		let { password, ...responseObj } = result[0][0]
+		return res.status(200).json(responseObj)
+	} catch (error) {
+		console.log(error)
+		return res
+			.status(500)
+			.json({ errors: [{ code: 500, message: 'Server error' }] })
+	}
 })
 
 // @route       POST api/auth/register
 // @desc        Register new user
 // @access      Public
 router.post(
-    '/register',
+	'/register',
 	[
 		body('first_name').isLength({ min: 2 }),
 		body('last_name').isLength({ min: 2 }),
 		body('email').isEmail(),
 		body('password').isLength({ min: 8, max: 64 }),
 	],
-    async (req, res) => {
-        // Checking for validation errors
+	async (req, res) => {
+		// Checking for validation errors
 		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
 			return res.status(422).json({
@@ -54,18 +57,16 @@ router.post(
 			})
 		}
 
-        try {
-            const {
-				first_name,
-				last_name,
-				email,
-				password,
-			} = req.body
+		try {
+			const { first_name, last_name, email, password } = req.body
 
-            const connection = await getConnection()
-            let result = await connection.query('SELECT * FROM app_user WHERE email = ?', [email])
-            if (result[0].length > 0) {
-                return res.status(422).json({
+			const connection = await getConnection()
+			let result = await connection.query(
+				'SELECT * FROM app_user WHERE email = ?',
+				[email]
+			)
+			if (result[0].length > 0) {
+				return res.status(422).json({
 					errors: [
 						{
 							code: 422,
@@ -73,33 +74,111 @@ router.post(
 						},
 					],
 				})
-            }
+			}
 
-            let userData = {
-                app_user_id: uuid.v4(),
-                first_name,
-                last_name,
-                email,
-                password: '',
-                app_user_access: 1,
-                register_option: 0,
-            }
+			let userData = {
+				app_user_id: uuid.v4(),
+				first_name,
+				last_name,
+				email,
+				password: '',
+				app_user_access: 1,
+				register_option: 0,
+			}
 
-            const salt = await bcrypt.genSalt(10)
+			const salt = await bcrypt.genSalt(10)
 			const hash = await bcrypt.hash(password, salt)
 
-            userData.password = hash
+			userData.password = hash
 
-            await connection.query('INSERT INTO app_user SET ?', userData)
-            connection.release()
+			await connection.query('INSERT INTO app_user SET ?', userData)
+			connection.release()
 
-            jwtPayload = {
+			jwtPayload = {
 				user: {
 					id: userData.app_user_id,
 				},
 			}
 
-            jwt.sign(
+			jwt.sign(
+				jwtPayload,
+				process.env.JWT_SECRET,
+				{ expiresIn: '3h' },
+				(err, token) => {
+					if (err) throw err
+					return res.status(201).json({ token })
+				}
+			)
+		} catch (error) {
+			console.log(error)
+			return res
+				.status(500)
+				.json({ errors: [{ code: 500, message: 'Server error' }] })
+		}
+	}
+)
+
+// @route       POST api/auth/login
+// @desc        Login a user
+// @access      Public
+router.post(
+	'/login',
+	[body('email').exists(), body('password').exists()],
+	async (req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(422).json({
+				errors: errors.array().map((x) => {
+					let { msg, ...new_x } = x
+					new_x['message'] = msg
+					new_x['code'] = 422
+					return new_x
+				}),
+			})
+		}
+
+		try {
+			const { email, password } = req.body
+
+			const connection = await getConnection()
+			let result = await connection.query(
+				'SELECT app_user_id, password, register_option FROM app_user WHERE email = ?',
+				[email]
+			)
+
+			if (result[0].length == 0) {
+				return res.status(400).json({
+					errors: [{ code: 400, message: 'Authorization failed!' }],
+				})
+			}
+
+			if (result[0][0].register_option != 0) {
+				return res.status(400).json({
+					errors: [
+						{
+							code: 400,
+							message: 'Incorrect register option!',
+						},
+					],
+				})
+			}
+
+			const isMatch = await bcrypt.compare(
+				password,
+				result[0][0].password
+			)
+			if (!isMatch) {
+				return res.status(400).json({
+					errors: [{ code: 400, message: 'Authorization failed!' }],
+				})
+			}
+
+			jwtPayload = {
+				user: {
+					id: result[0][0].app_user_id,
+				},
+			}
+			jwt.sign(
 				jwtPayload,
 				process.env.JWT_SECRET,
 				{ expiresIn: '3h' },
@@ -109,84 +188,15 @@ router.post(
 				}
 			)
 
-        } catch (error) {
-            console.log(error)
-            return res
+			connection.release()
+		} catch (error) {
+			console.log(error)
+			return res
 				.status(500)
 				.json({ errors: [{ code: 500, message: 'Server error' }] })
-        }
-    }
+		}
+	}
 )
-
-// @route       POST api/auth/login
-// @desc        Login a user
-// @access      Public
-router.post('/login', [body('email').exists(), body('password').exists()], async (req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(422).json({
-            errors: errors.array().map((x) => {
-                let { msg, ...new_x } = x
-                new_x['message'] = msg
-                new_x['code'] = 422
-                return new_x
-            }),
-        })
-    }
-    
-    try {
-        const { email, password } = req.body
-
-        const connection = await getConnection()
-        let result = await connection.query('SELECT app_user_id, password, register_option FROM app_user WHERE email = ?', [email])
-
-        if (result[0].length == 0) {
-            return res.status(400).json({
-                errors: [{ code: 400, message: 'Authorization failed!' }],
-            })
-        }
-
-        if (result[0][0].register_option != 0) {
-            return res.status(400).json({
-                errors: [
-                    {
-                        code: 400,
-                        message: 'Incorrect register option!',
-                    },
-                ],
-            })
-        }
-
-        const isMatch = await bcrypt.compare(password, result[0][0].password)
-        if (!isMatch) {
-            return res.status(400).json({
-                errors: [{ code: 400, message: 'Authorization failed!' }],
-            })
-        }
-
-        jwtPayload = {
-            user: {
-                id: result[0][0].app_user_id,
-            },
-        }
-        jwt.sign(
-            jwtPayload,
-            process.env.JWT_SECRET,
-            { expiresIn: '3h' },
-            (err, token) => {
-                if (err) throw err
-                return res.status(201).json({ token })
-            }
-        )
-
-        connection.release()
-    } catch (error) {
-        console.log(error)
-            return res
-				.status(500)
-				.json({ errors: [{ code: 500, message: 'Server error' }] })
-    }
-})
 
 // @route       GET api/auth/google/callback
 // @desc        Authenticate and authorize user via google account
@@ -205,7 +215,7 @@ router.post('/google/callback', async (req, res) => {
 		const { given_name, family_name, email, email_verified } = payload
 
 		const connection = await getConnection()
-		
+
 		const result = await connection.query(
 			'SELECT * FROM app_user WHERE app_user_id = ?',
 			[userid]
@@ -264,16 +274,19 @@ router.post('/google/callback', async (req, res) => {
 	}
 })
 
-
 // @route       PUT api/auth/edit-profile
 // @desc        Edit user profile information
 // @access      Private
-router.post('/edit-profile', [
-	body('first_name').exists(),
-	body('last_name').exists(),
-	body('notifications').exists(),
-	body('email').isEmail(),
-	authMiddleware], async (req, res) => {
+router.post(
+	'/edit-profile',
+	[
+		body('first_name').exists(),
+		body('last_name').exists(),
+		body('notifications').exists(),
+		body('email').isEmail(),
+		authMiddleware,
+	],
+	async (req, res) => {
 		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
 			return res.status(422).json({
@@ -285,40 +298,47 @@ router.post('/edit-profile', [
 				}),
 			})
 		}
-		
+
 		try {
 			const connection = await getConnection()
 			let newInfo = {
 				first_name: req.body.first_name,
 				last_name: req.body.last_name,
 				email: req.body.email,
-				notifications: req.body.notifications
+				notifications: req.body.notifications,
 			}
-	
+
 			if (req.body.password) {
 				if (req.body.password.length < 8) {
-					return res.status(400).json({errors: [
-						{
-							code: 400,
-							message: "New password invalid!"
-						}
-					]})
+					return res.status(400).json({
+						errors: [
+							{
+								code: 400,
+								message: 'New password invalid!',
+							},
+						],
+					})
 				}
 				let salt = await bcrypt.genSalt()
 				let hash = await bcrypt.hash(req.body.password, salt)
 				newInfo.password = hash
 				newInfo.register_option = 0
 			}
-			await connection.query('UPDATE app_user SET ? WHERE app_user_id = ?', [newInfo, req.user.id])
+			await connection.query(
+				'UPDATE app_user SET ? WHERE app_user_id = ?',
+				[newInfo, req.user.id]
+			)
 			connection.release()
-			return res.json({message: 'Profile details updated successfully!'})
+			return res.json({
+				message: 'Profile details updated successfully!',
+			})
 		} catch (error) {
 			console.log(error)
-				return res
-					.status(500)
-					.json({ errors: [{ code: 500, message: 'Server error' }] })
+			return res
+				.status(500)
+				.json({ errors: [{ code: 500, message: 'Server error' }] })
 		}
-	})
-	
+	}
+)
 
 module.exports = router
