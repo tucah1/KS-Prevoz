c1fba785a02658009f495078533d42e513e74ac9
Harun Tucakovic <harun.tucakovic1@gmail.com>
2021-04-25 14:39:42 +0200

    server default port fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-const PORT = process.env.WEBPORT || 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.json({ extended: false }))
 app.use(cors())
